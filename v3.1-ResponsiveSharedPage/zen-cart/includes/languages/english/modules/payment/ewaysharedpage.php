e85eab7ea69048e99703de011043e26e1868d379
Fayland Lam <fayland@gmail.com>
2013-11-06 21:00:27 +0800

    init zen-cart

---
+++ v3.1-ResponsiveSharedPage/zen-cart/includes/languages/english/modules/payment/ewaysharedpage.php
@@ -0,0 +1,7 @@
+<?php
+
+  define('MODULE_PAYMENT_EWAYRAPID_TEXT_TITLE', 'eWAY Responsive Shared Page');
+  define('MODULE_PAYMENT_EWAYRAPID_TEXT_PUBLIC_TITLE', 'eWAY Responsive Shared Page');
+  define('MODULE_PAYMENT_EWAYRAPID_TEXT_DESCRIPTION', 'Accepts payments with eWAY Responsive Shared Page');
+
+?>
