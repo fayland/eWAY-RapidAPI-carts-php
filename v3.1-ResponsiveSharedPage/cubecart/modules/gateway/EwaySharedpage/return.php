b529a91cbbc26e805f88b2230a7f031b30f2fe30
Fayland Lam <fayland@gmail.com>
2013-11-16 22:32:11 +0800

    init for cubecart shared page

---
+++ v3.1-ResponsiveSharedPage/cubecart/modules/gateway/EwaySharedpage/return.php
@@ -0,0 +1,12 @@
+<?php
+/* Chronopay malforms the return URL so we have to bring it to a *static* URL. */
+$AccessCode = $_GET['AccessCode'];
+if (isset($_GET['amp;AccessCode'])) $AccessCode = $_GET['amp;AccessCode'];
+$_GET = array(
+	'_g' => 'rm',
+	'type' => 'gateway',
+	'cmd' => 'process',
+	'module' => 'EwaySharedpage',
+	'AccessCode' => $AccessCode
+);
+require('..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'index.php');
